2dc17cf247c526161063b97b9dfc1664a353a7e7
Iain Gray <igray@itgassociates.com>
2013-03-14 00:05:21 +0000

    change ontabclick to pass event

--- assets/js/jquery.bootstrap.wizard.js
+++ assets/js/jquery.bootstrap.wizard.js
@@ -166,7 +166,7 @@ var bootstrapWizardCreate = function(element, options) {
 	obj.fixNavigationButtons();
 	
 	$('a[data-toggle="tab"]', element).on('click', function (e) {
-		if($settings.onTabClick && typeof $settings.onTabClick === 'function' && $settings.onTabClick($activeTab, $navigation, obj.currentIndex())===false){
+		if($settings.onTabClick && typeof $settings.onTabClick === 'function' && $settings.onTabClick(e, $activeTab, $navigation, obj.currentIndex())===false){
 		    return false;
 		}
 	});
